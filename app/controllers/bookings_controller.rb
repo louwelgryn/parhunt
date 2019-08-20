4143b40a8e0572066cd738803b0b186f24eed79c
Marie Stoppa <marie.stoppa@essec.edu>
2019-08-20 17:24:39 +0200

    fin review creation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,14 @@
 class BookingsController < ApplicationController
-  before_action :set_current_couple, :current_user
-
   def create
     @booking = Booking.new(booking_params)
     @booking.user = current_user
     @booking.couple = Couple.find(params[:couple_id])
     @booking.save!
-    redirect_to dashboard_path(current_user.dashboard)
+    redirect_to dashboard_path
   end
 
   def update
+    @booking = Booking.find(params[:id])
     if @booking.update(booking_params)
       redirect_to dashboard_path
     else
@@ -24,12 +23,6 @@ class BookingsController < ApplicationController
   end
 
   private
-
-  def set_current_couple
-    if params[:id]
-      @couple = Couple.find(params[:id])
-    end
-  end
 
   def booking_params
     params.require(:booking).permit(:user_id, :couple_id, :start_date, :end_date, :review_content, :review_rating, :status)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def dashboard
     @upcoming_bookings = current_user.bookings.where("start_date > ?", Date.today)
     @old_bookings = current_user.bookings.where("end_date < ?", Date.today)
-    # @old_bookings_to_review = @old_bookings.where("review_content == ?", nil)
+    @old_bookings_to_review = @old_bookings.where(review_content: nil)
   end
 end
