d50b67c7bf4e60d47d0854a8a516030d2e927262
Antoine Gaillard <antoine@agaillard.fr>
2019-08-22 16:32:04 +0200

    debug ok

--- app/models/couple.rb
+++ app/models/couple.rb
@@ -10,17 +10,20 @@ class Couple < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
 
   def average
-    return nil
-    # if self.bookings.first.present?
-    #   sum = 0
-    #   rated_bookings = self.bookings.where.not(review_rating: nil)
-    #   rated_bookings.each do |rated_booking|
-    #     sum += rated_booking.review_rating
-    #   end
-    #   avg_float = sum.fdiv(rated_bookings.count)
-    #   return (avg_float * 2).round / 2.0
-    # else
-    #   return nil
-    # end
+    if self.bookings.first.present?
+      sum = 0
+      rated_bookings = self.bookings.where.not(review_rating: nil)
+      if rated_bookings.first.present?
+        rated_bookings.each do |rated_booking|
+          sum += rated_booking.review_rating
+        end
+        avg_float = sum.fdiv(rated_bookings.count)
+        return (avg_float * 2).round / 2.0
+      else
+        return nil
+      end
+    else
+      return nil
+    end
   end
 end
