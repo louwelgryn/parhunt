7ae3411e7c67635eaed28c22749b146642653c5c
Marie Stoppa <marie.stoppa@essec.edu>
2019-08-22 17:59:25 +0200

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,11 @@ class UsersController < ApplicationController
     @old_bookings = current_user.bookings.where("end_date <= ?", Date.today)
     @old_bookings_to_review = @old_bookings.where(review_content: nil)
     @reviewed_old_bookings = @old_bookings.where.not(review_content: nil)
+
+    if current_user.couple.nil?
+      @bookings_to_validate = ""
+    else
+      @bookings_to_validate = current_user.couple.bookings.where("status = ?", "en attente")
+    end
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import "bootstrap";
 import "../plugins/flatpickr"
 import 'mapbox-gl/dist/mapbox-gl.css';
+// import 'circles.js'
 
 
 import '../plugins/smooth_scroll.js';
@@ -43,3 +44,20 @@ const calculator = () => {
 }
 
 calculator();
+
+
+/// RONDS NOTES ///
+const displays = document.querySelectorAll('.note-display');
+const transitionDuration = 900;
+
+displays.forEach(display => {
+  let progress = display.querySelector('.circle__progress--fill');
+  let radius = progress.r.baseVal.value;
+  let circumference = 2 * Math.PI * radius;
+
+  progress.style.setProperty('--transitionDuration', `${transitionDuration}ms`);
+  progress.style.setProperty('--initialStroke', circumference);
+
+  setTimeout(() => progress.style.strokeDashoffset = 50, 100);
+});
+
