129ac0c9b80710655a4624780f1b21d461d7a8c1
maxence ballande <maxence.ballande.16@neoma-bs.com>
2019-08-23 16:00:19 +0200

    navbar okayyyy

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,6 +1,8 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
-  if (navbar) {
+  const cover = document.querySelector('.landing-cover');
+  if (navbar && cover) {
+    navbar.classList.remove('navbar-lewagon-white');
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
         navbar.classList.add('navbar-lewagon-white');
